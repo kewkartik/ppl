34d3305e64f2a241b82808239c30400345fee8d4
KewKartik <108450560+KewKartik@users.noreply.github.com>
2023-08-27 18:40:31 +0530

    calc

--- api/index.py
+++ api/index.py
@@ -1,7 +1,42 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+app.add_middleware(
+    CORSMiddleware,
+    # Allow requests from any origin (not recommended for production)
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to the calculator API!"}
+
+
+@app.get("/api/add")  # Update the route decorator
+async def add_numbers(a: float, b: float):
+    result = a + b
+    return {"result": result}
+
+
+@app.get("/api/sub")  # Update the route decorator
+async def sub_numbers(a: float, b: float):
+    result = a - b
+    return {"result": result}
+
+
+@app.get("/api/mul")  # Update the route decorator
+async def add_numbers(a: float, b: float):
+    result = a * b
+    return {"result": result}
+
+
+@app.get("/api/div")  # Update the route decorator
+async def add_numbers(a: float, b: float):
+    result = a / b
+    return {"result": result}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,69 @@
 "use client"
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
-  const [message, setMessage] = useState("Loading");
-    useEffect(() => {
-      fetch("http://localhost:3000/api/python")
-        .then((response) => response.json())
-        .then((data) => {
-        setMessage(data.message);
-      });
-  }, []);
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleAdd = async () => {
+    const response = await fetch(`/api/add?a=${num1}&b=${num2}`);
+    const data = await response.json();
+    setResult(data.result);
+  };
+
+  const handleSub = async () => {
+    const response = await fetch(`/api/sub?a=${num1}&b=${num2}`);
+    const data = await response.json();
+    setResult(data.result);
+  };
+
+    const handleMul = async () => {
+    const response = await fetch(`/api/mul?a=${num1}&b=${num2}`);
+    const data = await response.json();
+    setResult(data.result);
+  };
+
+    const handleDiv = async () => {
+    const response = await fetch(`/api/div?a=${num1}&b=${num2}`);
+    const data = await response.json();
+    setResult(data.result);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py |</code>&nbsp;
-          </Link>
-          {message}
-        </p>
+          PPL&nbsp;
       </div>
+      <div className="grid grid-cols-1 gap-4 p-4">
+      <h1>Calculator</h1>
+      <Input
+        type="text"
+        placeholder="Enter number 1"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Enter number 2"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      />
+      <div className="flex gap-2">
+        <Button className="pr-24" variant="secondary" onClick={handleAdd}>Add</Button>
+        <Button className="pr-24" variant="secondary" onClick={handleSub}>Sub</Button>
+      </div>
+      <div className="flex gap-2">
+        <Button className="pr-24" variant="secondary" onClick={handleMul}>Mul</Button>
+        <Button className="pr-24" variant="secondary" onClick={handleDiv}>Div</Button>
+      </div>
+      <Button>
+      {result && <p>Result: {result}</p>}
+      </Button>
+    </div>
     </main>
   )
 }
