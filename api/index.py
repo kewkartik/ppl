82e070415d86082921a93d31fb6514fd4b610856
KewKartik <108450560+KewKartik@users.noreply.github.com>
2023-08-28 00:21:51 +0530

    update

--- api/index.py
+++ api/index.py
@@ -1,11 +1,10 @@
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # Allow requests from any origin (not recommended for production)
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
@@ -14,29 +13,37 @@ app.add_middleware(
 
 
 @app.get("/")
-async def read_root():
+async def read_root(request: Request):
     return {"message": "Welcome to the calculator API!"}
 
 
-@app.get("/api/add")  # Update the route decorator
+@app.get("/api")
+async def read_api_root():
+    return {"message": "/api/add, /api/sub, /api/mul, /api/div"}
+
+
+@app.get("/api/add")
 async def add_numbers(a: float, b: float):
     result = a + b
     return {"result": result}
 
 
-@app.get("/api/sub")  # Update the route decorator
+@app.get("/api/sub")
 async def sub_numbers(a: float, b: float):
     result = a - b
     return {"result": result}
 
 
-@app.get("/api/mul")  # Update the route decorator
-async def add_numbers(a: float, b: float):
+@app.get("/api/mul")
+async def mul_numbers(a: float, b: float):
     result = a * b
     return {"result": result}
 
 
-@app.get("/api/div")  # Update the route decorator
-async def add_numbers(a: float, b: float):
-    result = a / b
-    return {"result": result}
+@app.get("/api/div")
+async def div_numbers(a: float, b: float):
+    if b != 0:
+        result = a / b
+        return {"result": result}
+    else:
+        return {"error": "Division by zero is not allowed."}
